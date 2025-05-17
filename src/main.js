8e2354b8c5b97fce6cd639b8f2d32c5464cca3b7
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-05-17 13:41:28 +0200

    Update and rename main.jsx to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-// src/main.jsx
+// src/main.js
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // Importar o CSS
+import App from './App.js' // Importando como .js
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
