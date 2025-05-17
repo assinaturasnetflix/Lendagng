afd7fb2ff379ae34c87da1b53c07179d2793b850
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-05-17 14:04:55 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // src/main.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import App from './App.js';  // Certifique-se que o componente exportado em App.js se chama "App"
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
