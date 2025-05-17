21c07a5e95eb5b8f88e03e34ad4b5406ba4b4286
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-05-17 14:02:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // src/main.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js'; // Certifique-se que o nome aqui (App)
+import App from './App.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> {/* // <--- Corrigido para App (como importado e definido em App.js) */}
-  </React.StrictMode> {/* // <--- Vírgula removida */}
+    <App />
+  </React.StrictMode>
 );
