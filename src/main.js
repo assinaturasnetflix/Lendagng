580b64454aa8cdfcfe36817bdf32f0138741cc6d
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-05-17 14:21:06 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-// src/main.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';  // Certifique-se que o componente exportado em App.js se chama "App"
+import Aplicativo from './App.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(Aplicativo, null)
+  )
 );
